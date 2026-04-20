731277784d38f6a8b8997c219d04098a5e6debbd
Arnab Paul <arnabict@outlook.com>
2026-04-20 21:51:16 +0200

    feat: font change: quicksand

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Quicksand } from "next/font/google";
 import Navbar from "@/components/navbar";
+
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-quicksand",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${quicksand.variable} antialiased`}
       >
         <Navbar />
         {children}
