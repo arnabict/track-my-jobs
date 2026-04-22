f6ab04780093dfe5ec5c13334d4639f8ab72cb76
Arnab Paul <arnabict@outlook.com>
2026-04-22 12:59:00 +0200

    feat: set up better auth for authentication

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
 export default function SignUp() {
-  return <div>Signup</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
+      Signup
+    </div>
+  );
 }
