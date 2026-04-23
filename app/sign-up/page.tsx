e32d543d504a01f1efad55ca9dd6b950201efa8a
Arnab Paul <arnabict@outlook.com>
2026-04-23 14:10:01 +0200

    feat: sign-up page

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,9 +1,78 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
 export default function SignUp() {
   return (
     <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
-      Signup
+      <Card className="w-full max-w-md border-gray-200 shadow-lg">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold text-black">
+            Sign Up
+          </CardTitle>
+          <CardDescription className="text-gray-600">
+            Create an account to start tracking your job applications
+          </CardDescription>
+        </CardHeader>
+        <form className="space-y-4">
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="name" className="text-gray-700">
+                Name
+              </Label>
+              <Input
+                id="name"
+                type="text"
+                placeholder="John Doe"
+                required
+                className="border-gray-300 focus:border-primary focus:ring-primary"
+              ></Input>
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="john@example.com"
+                required
+                className="border-gray-300 focus:border-primary focus:ring-primary"
+              ></Input>
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                required
+                minLength={8}
+                className="border-gray-300 focus:border-primary focus:ring-primary"
+              ></Input>
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col space-y-4">
+            <Button
+              type="submit"
+              className="w-full bg-primary hover:bg-primary/90 cursor-pointer"
+            >
+              Sign Up
+            </Button>
+            <p>
+              Already have an account? <Link href="/sign-in">Sign In</Link>
+            </p>
+          </CardFooter>
+        </form>
+      </Card>
     </div>
   );
 }
