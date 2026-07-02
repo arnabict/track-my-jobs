dd0c7c7683d44c1a126b0cd1f8db68a032ed7007
Arnab Paul <arnabict@outlook.com>
2026-07-02 21:02:47 +0200

    feat: job application card

--- components/job-application-card.tsx
+++ components/job-application-card.tsx
@@ -1,4 +1,13 @@
 import { Column, JobApplication } from "@/lib/models/models.types";
+import { Card, CardContent } from "./ui/card";
+import { Edit2, ExternalLink, MoreVertical } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
 
 interface JobApplicationCardProps {
   job: JobApplication;
@@ -9,5 +18,60 @@ export default function JobApplicationCard({
   job,
   columns,
 }: JobApplicationCardProps) {
-  return <div>JobApplicationCard</div>;
+  return (
+    <>
+      <Card>
+        <CardContent>
+          <div>
+            <div>
+              <h3>{job.position}</h3>
+              <p>{job.company}</p>
+              {job.description && <p>{job.description}</p>}
+              {job.tags && job.tags.length > 0 && (
+                <div>
+                  {job.tags.map((tag, key) => (
+                    <span key={key}>{tag}</span>
+                  ))}
+                </div>
+              )}
+              {job.jobUrl && (
+                <a
+                  target="_blank"
+                  href={job.jobUrl}
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <ExternalLink />
+                </a>
+              )}
+            </div>
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button variant="ghost" size="icon">
+                    <MoreVertical />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem>
+                    <Edit2 /> Edit
+                  </DropdownMenuItem>
+                  {columns.length > 1 && (
+                    <>
+                      {columns
+                        .filter((c) => c._id !== job.columnId)
+                        .map((column) => (
+                          <DropdownMenuItem>
+                            Move to {column.name}
+                          </DropdownMenuItem>
+                        ))}
+                    </>
+                  )}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
