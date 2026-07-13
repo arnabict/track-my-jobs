dc3795e0bc31651c09d7efe801630c60a78c603c
Arnab Paul <arnabict@outlook.com>
2026-07-13 14:19:09 +0200

    feat: caching in dashboard component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,32 +2,38 @@ import KanbanBoard from "@/components/kanban-board";
 import { getSession } from "@/lib/auth/auth";
 import connectDB from "@/lib/db";
 import { Board } from "@/lib/models";
-import type { Board as BoardType } from "@/lib/models/models.types";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
-export default async function Dashboard() {
+async function getBoard(userId: string) {
+  "use cache";
+
+  await connectDB();
+
+  const boardDoc = await Board.findOne({
+    userId: userId,
+    name: "Job Hunt",
+  }).populate({
+    path: "columns",
+    populate: {
+      path: "jobApplications",
+    },
+  });
+
+  if (!boardDoc) return null;
+
+  const board = JSON.parse(JSON.stringify(boardDoc));
+
+  return board;
+}
+
+async function DashboardPage() {
   const session = await getSession();
+  const board = await getBoard(session?.user.id ?? "");
 
   if (!session?.user) {
     redirect("/sign-in");
   }
-
-  await connectDB();
-
-  const board = await Board.findOne({
-    userId: session.user.id,
-    name: "Job Hunt",
-  })
-    .populate({
-      path: "columns",
-    })
-    .lean();
-
-  if (!board) {
-    redirect("/");
-  }
-
-  const plainBoard = JSON.parse(JSON.stringify(board)) as BoardType;
 
   return (
     <div className="min-h-screen bg-white">
@@ -36,8 +42,16 @@ export default async function Dashboard() {
           <h1 className="text-3xl font-bold text-black">Job Hunt</h1>
           <p className="text-gray-600">Track your job applications</p>
         </div>
-        <KanbanBoard board={plainBoard} userId={session.user.id} />
+        <KanbanBoard board={board} userId={session.user.id} />
       </div>
     </div>
+  );
+}
+
+export default async function Dashboard() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <DashboardPage />;
+    </Suspense>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  cacheComponents: true,
 };
 
 export default withSentryConfig(nextConfig, {
@@ -40,5 +40,5 @@ export default withSentryConfig(nextConfig, {
       // Automatically tree-shake Sentry logger statements to reduce bundle size
       removeDebugLogging: true,
     },
-  }
+  },
 });
